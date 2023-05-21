f3bda354105e33045893c338ea4e243a2f83b253
Nguyễn ThànhMinh <minhnguyen16082001@gmail.com>
2023-05-21 11:58:14 +0700

    Create API

--- Assgment_Nhom3_WebBanDienThoai/IServices/IBoNhoTrongServices.cs
+++ Assgment_Nhom3_WebBanDienThoai/IServices/IBoNhoTrongServices.cs
@@ -1,6 +1,14 @@
-﻿namespace Assgment_Nhom3_WebBanDienThoai.IServices
+﻿using Assgment_Nhom3_WebBanDienThoai.Models;
+namespace Assgment_Nhom3_WebBanDienThoai.IServices
 {
     public interface IBoNhoTrongServices
     {
+        public bool CreateBoNhoTrong(BoNhoTrong p);
+        public bool UpdateChatLieu(BoNhoTrong p);
+        public bool DeleteChatLieu(Guid id);
+        public List<BoNhoTrong> GetAllBoNhoTrongs();
+        public BoNhoTrong GetBoNhoTrongsById(Guid id);
+
+        public List<BoNhoTrong> GetBoNhoTrongsByName(string name);
     }
 }
--- Assgment_Nhom3_WebBanDienThoai/IServices/IRamServices.cs
+++ Assgment_Nhom3_WebBanDienThoai/IServices/IRamServices.cs
@@ -1,6 +1,14 @@
-﻿namespace Assgment_Nhom3_WebBanDienThoai.IServices
+﻿using Assgment_Nhom3_WebBanDienThoai.Models;
+namespace Assgment_Nhom3_WebBanDienThoai.IServices
 {
     public interface IRamServices
     {
+        public bool CreateRam(Ram p);
+        public bool UpdateRam(Ram p);
+        public bool DeleteRam(Guid id);
+        public List<Ram> GetAllRams();
+        public Ram GetRamsById(Guid id);
+
+        public List<Ram> GetRamsByName(string name);
     }
 }
