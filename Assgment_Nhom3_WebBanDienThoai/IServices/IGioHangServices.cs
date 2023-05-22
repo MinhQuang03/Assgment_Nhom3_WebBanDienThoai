a0d06a282651345526fc03ca1a4e117cb883c698
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2023-05-22 23:46:31 +0700

    add service HinhThucThanhToan

---
+++ Assgment_Nhom3_WebBanDienThoai/IServices/IGioHangServices.cs
@@ -0,0 +1,12 @@
+﻿using Assgment_Nhom3_WebBanDienThoai.Models;
+
+namespace Assgment_Nhom3_WebBanDienThoai.IServices
+{
+    public interface IGioHangServices
+    {
+        public bool Create(GioHang obj);
+        public bool Delete(Guid id);
+        public bool Update(GioHang obj);
+        public List<GioHang> GetAll();
+    }
+}
