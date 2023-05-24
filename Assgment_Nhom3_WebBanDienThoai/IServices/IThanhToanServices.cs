36674b862e5a3c5ce7facc52a4b0de487204c0cd
Nguyễn ThànhMinh <minhnguyen16082001@gmail.com>
2023-05-25 01:23:26 +0700

    2 Thanh Toan

--- Assgment_Nhom3_WebBanDienThoai/IServices/IThanhToanServices.cs
+++ Assgment_Nhom3_WebBanDienThoai/IServices/IThanhToanServices.cs
@@ -1,6 +1,12 @@
-﻿namespace Assgment_Nhom3_WebBanDienThoai.IServices
+﻿using Assgment_Nhom3_WebBanDienThoai.Models;
+
+namespace Assgment_Nhom3_WebBanDienThoai.IServices
 {
     public interface IThanhToanServices
     {
+        public bool CreateThanhToan(ThanhToan tt);
+        public bool DeleteThanhToan(Guid id);
+        public bool UpdateThanhToan(ThanhToan tt);
+        public List<ThanhToan> GetAll();
     }
 }
--- Assgment_Nhom3_WebBanDienThoai/Models/ShoppingDbContext.cs
+++ Assgment_Nhom3_WebBanDienThoai/Models/ShoppingDbContext.cs
@@ -66,7 +66,7 @@ namespace Assgment_Nhom3_WebBanDienThoai.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            optionsBuilder.UseSqlServer(@"Data Source=MSI;Initial Catalog=BanDienThoai_Nhom2;User ID=thanhnxph20424;Password=05012003  ;TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-H6TL71T\MINGG;Initial Catalog=BanDienThoai_Nhom2;User ID=minh160801;Password=minhanh321  ;TrustServerCertificate=True");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
