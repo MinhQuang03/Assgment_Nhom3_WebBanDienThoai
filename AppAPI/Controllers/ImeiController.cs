03bd2320e1db35083523d0967e4a54f0bb77a01d
2 am Quang <86760575+MinhQuang03@users.noreply.github.com>
2023-05-24 21:32:11 +0700

    imei

---
+++ AppAPI/Controllers/ImeiController.cs
@@ -0,0 +1,70 @@
+﻿using Assgment_Nhom3_WebBanDienThoai.IServices;
+using Assgment_Nhom3_WebBanDienThoai.Models;
+using Assgment_Nhom3_WebBanDienThoai.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImeiController : ControllerBase
+    {
+        public IImeiServices imeiServices { get; set; }
+
+        public ImeiController()
+        {
+            imeiServices = new ImeiServices();
+        }
+        // GET: api/<ImeiController>
+        [HttpGet]
+        [HttpGet("get-all-Imei")]
+        public List<Imei> Get()
+        {
+            return imeiServices.GetAll();
+        }
+
+        // GET api/<ImeiController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<ImeiController>
+        [HttpPost("create-imei")]
+        public bool Create(string ten, int TrangThai, Guid IdCtsp)
+        {
+            Imei a = new Imei()
+            {
+                Id = Guid.NewGuid(),
+                imei = ten,
+                TrangThai = TrangThai,
+                IdCtsp = IdCtsp
+            };
+            return imeiServices.Create(a);
+        }
+
+        // PUT api/<ImeiController>/5
+        [HttpPut("update-imei-{id}")]
+        public bool update(Guid id, [FromBody] string ten, int TrangThai, Guid IdCtsp)
+        {
+            Imei a = new Imei()
+            {
+                Id = id,
+                imei = ten,
+                TrangThai = TrangThai,
+                IdCtsp = IdCtsp
+            };
+            return imeiServices.Update(a);
+        }
+
+        // DELETE api/<ImeiController>/5
+        [HttpDelete("delete-imei-{id}")]
+        public bool Delete(Guid id)
+        {
+            return imeiServices.Delete(id);
+        }
+    }
+}
