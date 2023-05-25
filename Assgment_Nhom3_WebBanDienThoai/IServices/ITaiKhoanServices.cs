7eb7703b34c9cb7533a6d011df75d45eb873c6f6
nguyenquangtai03 <93028729+taizbaby@users.noreply.github.com>
2023-05-25 11:01:32 +0700

    a

--- Assgment_Nhom3_WebBanDienThoai/IServices/ITaiKhoanServices.cs
+++ Assgment_Nhom3_WebBanDienThoai/IServices/ITaiKhoanServices.cs
@@ -8,5 +8,8 @@ namespace Assgment_Nhom3_WebBanDienThoai.IServices
         public bool DeleteTaiKhoan(Guid id);
         public bool UpdateTaiKhoan(TaiKhoan tk);
         public List<TaiKhoan> GetAll();
+
+        public TaiKhoan GetSanPhamById(Guid id);
+        public List<TaiKhoan> GetTaiKhoanByName(string name);
     }
 }
