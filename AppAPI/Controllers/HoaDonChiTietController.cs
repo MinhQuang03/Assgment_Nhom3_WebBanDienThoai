af7471f677c4ecaad3ed81411b834bec2a6b6fef
KhanhLK99 <106392405+KhanhLK99@users.noreply.github.com>
2023-05-27 10:40:57 +0700

    add hdct

--- AppAPI/Controllers/HoaDonChiTietController.cs
+++ AppAPI/Controllers/HoaDonChiTietController.cs
@@ -2,6 +2,7 @@
 using Assgment_Nhom3_WebBanDienThoai.Models;
 using Assgment_Nhom3_WebBanDienThoai.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,9 +13,13 @@ namespace AppAPI.Controllers
     public class HoaDonChiTietController : ControllerBase
     {
         private readonly IHoaDonChiTietServices _hoaDonChiTiet;
+        private IChiTietSanPhamServices ctsp;
+        private IImeiServices mei;
         ShoppingDbContext context = new ShoppingDbContext();
         public HoaDonChiTietController()
         {
+            mei = new ImeiServices();
+            ctsp = new  ChiTietSanPhamServices();
             _hoaDonChiTiet = new HoaDonChiTietServices();
         }
         // GET: api/<HoaDonChiTietController>
@@ -26,15 +31,42 @@ namespace AppAPI.Controllers
 
         // GET api/<HoaDonChiTietController>/5
         [HttpPost("Create-HoaDonChiTiet")]
-        public bool CreateHoaDonChiTiet(Guid IdHoaDon, Guid IdChiTietSp, int SoLuong, long Gia, int TrangThai)
+        public bool CreateHoaDonChiTiet(Guid IdHoaDon, Guid IdChiTietSp, int SoLuong, int TrangThai)
         {
-            HoaDonChiTiet hoaDonChiTiet = new HoaDonChiTiet();
-            hoaDonChiTiet.Id = Guid.NewGuid();
-            hoaDonChiTiet.IdHoaDon = IdHoaDon;
-            hoaDonChiTiet.IdChiTietSp = IdChiTietSp;
-            hoaDonChiTiet.SoLuong = SoLuong;
-            hoaDonChiTiet.TrangThai = TrangThai;
-            return _hoaDonChiTiet.CreateHoaDonChiTiet(hoaDonChiTiet);
+            var a = mei.GetAll().Where(a=>a.IdCtsp == IdChiTietSp).ToList();
+            var ct = ctsp.GetAll().FirstOrDefault(a => a.Id == IdChiTietSp);
+            var hd = _hoaDonChiTiet.GetAllHoaDonChiTiets().FirstOrDefault(a=>a.IdChiTietSp == IdChiTietSp && a.IdHoaDon ==  IdHoaDon  );
+            if (SoLuong > a.Count)
+            {
+                BadRequest("soluong qua lon");
+                return false;
+            }
+            else
+            {
+                if (hd == null)
+                {
+                    HoaDonChiTiet hoaDonChiTiet = new HoaDonChiTiet();
+                    hoaDonChiTiet.Id = Guid.NewGuid();
+                    hoaDonChiTiet.IdHoaDon = IdHoaDon;
+                    hoaDonChiTiet.IdChiTietSp = IdChiTietSp;
+                    hoaDonChiTiet.SoLuong = SoLuong;
+                    hoaDonChiTiet.Gia = Convert.ToInt64(ctsp.GetAll().FirstOrDefault(a => a.Id == IdChiTietSp).DonGia);
+                    hoaDonChiTiet.TrangThai = TrangThai;
+                    return _hoaDonChiTiet.CreateHoaDonChiTiet(hoaDonChiTiet);
+
+                }
+                else
+                {
+
+                    var hds = _hoaDonChiTiet.GetAllHoaDonChiTiets().FirstOrDefault(a => a.IdChiTietSp == IdChiTietSp && a.IdHoaDon == IdHoaDon);
+                    hds.SoLuong += SoLuong;
+                    hds.Gia = Convert.ToInt64(ct.DonGia * hds.SoLuong);
+                   
+                    return _hoaDonChiTiet.UpdateHoaDonChiTiet(hds);
+                }
+
+            }
+
 
 
         }
