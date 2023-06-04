f2be6bdd306856fd069eba354589ac62c64dc2c7
2 am Quang <86760575+MinhQuang03@users.noreply.github.com>
2023-06-04 20:09:08 +0700

    comintt

--- Assgment_Nhom3_WebBanDienThoai/Areas/Admin/Controllers/GiamGiaController.cs
+++ Assgment_Nhom3_WebBanDienThoai/Areas/Admin/Controllers/GiamGiaController.cs
@@ -20,7 +20,7 @@ namespace Assgment_Nhom3_WebBanDienThoai.Areas.Admin.Controllers
             List<GiamGia> giamGias = JsonConvert.DeserializeObject<List<GiamGia>>(datajson);
             return View(giamGias);
         }
-       
+
         public IActionResult Create()
         {
             return View();
@@ -29,19 +29,50 @@ namespace Assgment_Nhom3_WebBanDienThoai.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(GiamGia gg)
         {
-            string jsonData = JsonConvert.SerializeObject(gg);
-            HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.PostAsync("https://localhost:7151/api/GiamGia/create-MaGiamGia", content);
-            if (response.IsSuccessStatusCode)
+            try
             {
-                return RedirectToAction("Index");
+                string jsonData = JsonConvert.SerializeObject(gg);
+                HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("https://localhost:7151/api/GiamGia/create-MaGiamGia", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "Them thanh cong";
+                    return RedirectToAction("Index");
+                }
             }
-
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
             return View();
         }
 
-        
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            GiamGia gg = new GiamGia();
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7151/api/GiamGia/Get" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                gg = JsonConvert.DeserializeObject<GiamGia>(data);
+            }
+            return View(gg);
+        }
 
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(GiamGia gg)
+        {
+            string data = JsonConvert.SerializeObject(gg);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PutAsync("https://localhost:7151/api/GiamGia/update", content);
+            if (response.IsSuccessStatusCode) 
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
